68c20cce3d504b53999f6fa13761decc250c8d13
Pedro Arthur <pedroarthurdr@hotmail.com>
2022-06-12 17:58:55 -0300

    selecionados visual pronto

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 
 function selecionar1(elemento) {
-
-    const pratoescolhido = document.querySelector(".selecionado");
+    const pratoescolhido = document.querySelector(".selecionadoP");
     if (pratoescolhido !== null) {
-      pratoescolhido.classList.remove("selecionado");
+      pratoescolhido.classList.remove("selecionadoP");
+      document.querySelector(".escondido1").classList.remove("escondido1");
     }
 
-    elemento.classList.add("selecionado");
+    elemento.classList.add("selecionadoP");
+    elemento.querySelector(".verif").classList.toggle("escondido1");
   }
 
   function selecionar2(elemento) {
-    const bebidacolhida = document.querySelector(" .bebida .selecionado");
+    const bebidacolhida = document.querySelector(" .selecionadoB");
     if (bebidacolhida !== null) {
-        bebidacolhida.classList.remove("selecionado");
+        bebidacolhida.classList.remove("selecionadoB");
+        document.querySelector(".escondido2").classList.remove("escondido2");
     }
 
-    elemento.classList.add("selecionado");
+    elemento.classList.add("selecionadoB");
+    elemento.querySelector(".verif").classList.toggle("escondido2");
   }
 
   function selecionar3(elemento) {
-    const sobremesacolhida = document.querySelector(" .sobremesa .selecionado");
+    const sobremesacolhida = document.querySelector(" .selecionadoS");
     if (sobremesacolhida !== null) {
-        sobremesacolhida.classList.remove("selecionado");
+        sobremesacolhida.classList.remove("selecionadoS");
+        document.querySelector(".escondido3").classList.remove("escondido3");
     }
 
-    elemento.classList.add("selecionado");
+    elemento.classList.add("selecionadoS");
+    elemento.querySelector(".verif").classList.toggle("escondido3");
   }
